bdd45e147aac2f1538d93dd2ea61a7401bf31a31
alua-gif <aluadzumas@gmail.com>
2026-03-30 17:31:10 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+host = "localhost"
+user = "postgres"
+password = "фдгф2025"
+db_name = "postgres"
+port = 5432
---
+++ connect.py
@@ -0,0 +1,17 @@
+import psycopg2
+
+def connect():
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="postgres",
+        user="postgres",
+        password="фдгф2025"
+    )
+    print("CONNECTED")
+    return conn
+
+
+
+
+
